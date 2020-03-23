14eaf7011be3207499beba7564037aa98c617164
“Nayonna <Nayonna Purnell>
2020-03-23 08:05:51 -0400

    Completed the challenge needs refactoring

---
+++ while-loops.py
@@ -0,0 +1,42 @@
+#Ask the user if they understand Python While loops
+
+
+answer = "NO"
+keepGoing = True
+
+name = input("What's your name? ")
+print(f"{name}, do you understand Python while loops?")
+comprehensionResponse = input("Enter yes/no: ")
+comprehensionResponse = comprehensionResponse.upper()
+
+
+while comprehensionResponse == answer:
+        print(f"Ok, {name}, while loops in Python repeat as long as a certain Boolean condition is met.")
+        print(f"{name}, now do you understand Python while loops?")
+        comprehensionResponse = input("Enter yes/no: ")
+        comprehensionResponse = comprehensionResponse.upper()
+
+        if comprehensionResponse == answer:
+            print(f"{name}, now do you understand Python while loops?")
+        else:
+            print(f"That's great, {name}.  I'm pleased that you understand while loops now.  That was getting repetitive.")
+
+print(f"That's great, {name}.  I'm pleased that you understand while loops now.  That was getting repetitive.")
+
+
+        
+       
+       
+
+       
+
+
+
+#Ask the user if they understand Python While loops
+   #If no, print("Ok, {name}, while loops in Python repeat as long as
+   # a certain Boolean condition is met.")
+
+
+    #if yes, they understand Python loops
+    #print "That's great, {name}.  I'm pleased that you understand
+    # while loops now.  That was getting repetitive."
